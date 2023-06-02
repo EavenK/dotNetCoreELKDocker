85736b86c88b848dd2d0b03b3bb1bf224ba46f01
EavenKhaw <cykhaw@hotmail.com>
2023-06-02 13:43:00 +0530

    Add files via upload

---
+++ EcommerceConsoleApp/EcommerceConsoleApp/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceConsoleApp.Models; 
+
+public record Category
+{
+    //initialized at constructor level
+    public long CategoryId { get; init; }
+    public string? CategoryName { get; init; }
+    //nullability test
+    public DateTime? CreatedDate { get; init; }
+    public DateTime? ModifiedDate { get; set; }
+    public string? CreatedBy { get; init; }
+    public string? ModifiedBy { get; set; }
+    public bool? IsActive { get; set; }
+
+
+}
+
+//immutable
+public record Category1(long CategoryId, string? CategoryName, string? CreatedBy, DateTime? CreatedDate );
---
+++ EcommerceConsoleApp/EcommerceConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Top Level Statement
+global using EcommerceConsoleApp.Models;
+using System.Data.Common;
+
+Console.WriteLine("Hello, World! .Net Core 7");
+
+//instance for Category
+var electronicCategory = new Category
+{
+    CategoryId = new Random().NextInt64(10000),
+    CategoryName = "Electronics",
+    CreatedBy = "User1",
+    CreatedDate = new DateTime(2023, 1, 1),
+    IsActive = true
+};
+
+//init cannot
+//set
+electronicCategory.IsActive = false;
+electronicCategory.ModifiedBy = "user2";
+electronicCategory.ModifiedDate = new DateTime(2023, 5, 5);
+
+
+var furnitureCategory = electronicCategory with 
+{
+    CategoryId = new Random().NextInt64(20000),
+    CategoryName = "Furniture"
+
+};
+
+Console.WriteLine($"Eletronic Category {electronicCategory}");
+
+Console.WriteLine($"Furniture Category {furnitureCategory}");
+
+
+var elecCategory = new Category1
+(
+    new Random().NextInt64(10000),
+    "Electronics",
+    "User1",
+    DateTime.Now
+);
+//immutability - cannot be mutable with this () of declaration in Models 
+//elecCategory.CreatedBy = "user2";
+Console.WriteLine($"Elec Category {elecCategory}");
+
+//Line Feed 
+long month = new Random().NextInt64(11);
+Console.WriteLine($"Month is {month}");
+string Season = $"The season is { month switch
+{ 
+    1 or 2 or 12 => "winter",
+    >2 and <6 => "spring",
+    >5 and <9 => "summer",
+    >8 and <12 => "autumn",
+    _ => "Unknown. Wrong Month Number",
+
+}}";
+
+Console.WriteLine(Season);
+
+Console.ReadKey();
